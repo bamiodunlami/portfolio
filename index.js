e32f53b35de1af386159725b70d2282e5e875d6e
Bami odunlami <odunlamibamidelejohn@gmail.com>
2022-07-10 11:15:34 -0700

    First commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
         console.log('is_checked: ' + is_checked); 
 
         if (is_checked==true){
-                $(".form-check-label").text("Light Mode")
+                // $(".form-check-label").text(" Dark mode")
             $("<link/>", {
                 rel: "stylesheet",
                 type: "text/css",
@@ -23,7 +23,7 @@ $(document).ready(function(){
              }).appendTo("head");
 
         } else{
-            $(".form-check-label").text("Dark Mode")
+            // $(".form-check-label").text("Light theme")
             $("<link/>", {
                 rel: "stylesheet",
                 type: "text/css",
