d7301c288b86b200d5fcba2897984eb6740d92d2
Bamidele Odunlami <bamideleodunlami@Bamideles-MacBook-Pro.local>
2023-04-06 12:52:43 +0100

    First Commit

--- index.js
+++ index.js
@@ -15,36 +15,36 @@ $(document).ready(function(){
         $(".section-three").show();
         });
 
-    // val= $('#flexSwitchCheckChecked');
-    // // $('#flexSwitchCheckChecked').change('click', function(){
-    // //     console.log(typeof($(val).val()));
-    // // });
-    // // if($('#checkbox1').prop('checked', true).change()){
-    // //     alert("ok");
-    // // }
-
-    // $('#flexSwitchCheckChecked').click(function() 
-    // {
-    //     var is_checked = $(this).is(':checked');
-    //     console.log('is_checked: ' + is_checked); 
-
-    //     if (is_checked==true){
-    //             // $(".form-check-label").text(" Dark mode")
-    //         $("<link/>", {
-    //             rel: "stylesheet",
-    //             type: "text/css",
-    //             href: "dark.css"
-    //          }).appendTo("head");
-
-    //     } else{
-    //         // $(".form-check-label").text("Light theme")
-    //         $("<link/>", {
-    //             rel: "stylesheet",
-    //             type: "text/css",
-    //             href: "style.css"
-    //          }).appendTo("head");
-    //     }
+    val= $('#flexSwitchCheckChecked');
+    // $('#flexSwitchCheckChecked').change('click', function(){
+    //     console.log(typeof($(val).val()));
     // });
+    // if($('#checkbox1').prop('checked', true).change()){
+    //     alert("ok");
+    // }
+
+    $('#flexSwitchCheckChecked').click(function() 
+    {
+        var is_checked = $(this).is(':checked');
+        console.log('is_checked: ' + is_checked); 
+
+        if (is_checked==true){
+                // $(".form-check-label").text(" Dark mode")
+            $("<link/>", {
+                rel: "stylesheet",
+                type: "text/css",
+                href: "dark.css"
+             }).appendTo("head");
+
+        } else{
+            // $(".form-check-label").text("Light theme")
+            $("<link/>", {
+                rel: "stylesheet",
+                type: "text/css",
+                href: "style.css"
+             }).appendTo("head");
+        }
+    });
 
     //slide effects
 
